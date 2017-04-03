464885184f6e7730dd107b7d44ad3fffb5508989
htwright <harrisontwright@gmail.com>
2017-04-03 09:28:23 -0500

    added string interpolation, default values'

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log("Hello, World!");
+
+function sayHello(firstName = 'Harrison', lastName = 'Wright'){
+	return `He said,"${firstName}, how are you?"`;
+};
+
+var greeting = sayHello('Harrison', 'Wright');
+console.log(greeting);
